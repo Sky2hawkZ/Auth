b6ad2c44454096dcc4cca9c81d31f4f0d45f459a
Hangfish <dan.lofgrens@gmail.com>
2019-04-14 00:14:49 +0200

    Updated App.js
    Fixed imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header, Button, Spinner, Card, CardSection } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
